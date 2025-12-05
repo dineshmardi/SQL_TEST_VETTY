bb4fd58b6637b938a928d8f1f76d2ea885ea5b8e
DINESH MARDI <132223103+dineshmardi@users.noreply.github.com>
2025-12-05 13:54:23 +0000

    added Q2 - stores with at least 5 orders in Oct 2020

--- answer.sql
+++ answer.sql
@@ -7,3 +7,15 @@ FROM transactions
 WHERE refund_item IS NULL
 GROUP BY DATE_FORMAT(purchase_time, '%Y-%m')
 ORDER BY purchase_month;
+
+
+/*Q2: Number of stores receiving at least 5 orders in Oct 2020 */
+
+SELECT 
+    store_id,
+    COUNT(*) AS order_count
+FROM transactions
+WHERE purchase_time BETWEEN '2020-10-01' AND '2020-10-31 23:59:59'
+GROUP BY store_id
+HAVING COUNT(*) >= 5;
+
