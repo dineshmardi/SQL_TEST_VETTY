763723322598ac12dafe341ad062d31457788005
DINESH MARDI <132223103+dineshmardi@users.noreply.github.com>
2025-12-05 13:56:20 +0000

    Q3 added - shortest refund processing interval per store

--- answer.sql
+++ answer.sql
@@ -19,3 +19,14 @@ WHERE purchase_time BETWEEN '2020-10-01' AND '2020-10-31 23:59:59'
 GROUP BY store_id
 HAVING COUNT(*) >= 5;
 
+
+/*  Q3: Shortest interval (in minutes) from purchase to refund*/
+/* Consider only refunded transactions (refund_time IS NOT NULL).*/
+SELECT 
+    store_id,
+    MIN(TIMESTAMPDIFF(MINUTE, purchase_time, refund_time)) 
+        AS shortest_refund_interval_minutes
+FROM transactions
+WHERE refund_time IS NOT NULL
+GROUP BY store_id;
+
